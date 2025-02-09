a184844540349c436826c5371d01ccdad3df522b
Prabal Bhandary <prabalbhandary97@gmail.com>
2025-02-09 15:59:13 +0545

    Attendance Records Delete while deleting the Department

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,14 +2,31 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    role: { type: String, enum: ["admin", "employee"], required: true },
-    profileImage: { type: String, default: "" },
-    profileImagePublicId: { type: String, default: "" },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "employee"],
+      required: true,
+    },
+    profileImage: {
+      type: String,
+      default: "",
+    },
   },
   { timestamps: true }
 );
 
-export default mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+export default User;
